3fca1f8616dc25ad2efd56c78231d9f9f18499f5
Mauricio Souza <48973883+mmdsza@users.noreply.github.com>
2023-02-28 10:19:46 -0300

    Minor fixes to code structure

--- startup.py
+++ startup.py
@@ -10,25 +10,13 @@ from time import sleep
 def test_sample_test():
     chrome_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-    chrome_driver.get('https://lambdatest.github.io/sample-todo-app/')
+    chrome_driver.get('randomwebsite')
     chrome_driver.maximize_window()
  
-    chrome_driver.find_element_by_name("li1").click()
-    chrome_driver.find_element_by_name("li2").click()
+    chrome_driver.find_element_by_XXX
  
-    title = "Sample page - lambdatest.com"
+    title = "Any title really, assertion test"
     assert title == chrome_driver.title
  
-    sample_text = "Happy Testing at LambdaTest"
-    email_text_field = chrome_driver.find_element_by_id("sampletodotext")
-    email_text_field.send_keys(sample_text)
-    sleep(5)
- 
-    chrome_driver.find_element_by_id("addbutton").click()
-    sleep(5)
- 
-    output_str = chrome_driver.find_element_by_name("li6").text
-    sys.stderr.write(output_str)
     
-    sleep(2)
     chrome_driver.close()
